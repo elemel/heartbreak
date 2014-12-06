421fd6de5bc86285ad83e2db1d5eebee7bc2031f
Mikael Lind <elemel@elemel.se>
2014-12-06 15:15:08 +0100

    Add wall model

---
+++ WallModel.lua
@@ -0,0 +1,47 @@
+local WallModel = {}
+WallModel.__index = WallModel
+
+function WallModel.new(game, id, config)
+    local model = {}
+    setmetatable(model, WallModel)
+
+    model._game = game
+    model._id = id
+    model._config = config or {}
+
+    return model
+end
+
+function WallModel:getType()
+    return "wall"
+end
+
+function WallModel:getId()
+    return self._id
+end
+
+function WallModel:getConfig()
+    return self._config
+end
+
+function WallModel:create()
+    local config = self._config
+
+    local world = self._game:getWorld()
+    local x, y = unpack(config.position or {0, 0})
+    self._body = love.physics.newBody(world, x, y, "static")
+
+    local width, height = unpack(config.size or {1, 1})
+    local shape = love.physics.newRectangleShape(width, height)
+    self._fixture = love.physics.newFixture(self._body, shape)
+end
+
+function WallModel:destroy()
+    self._fixture:destroy()
+    self._body:destroy()
+end
+
+function WallModel:update(dt)
+end
+
+return WallModel
--- main.lua
+++ main.lua
@@ -1,13 +1,30 @@
 local BallModel = require "BallModel"
 local Game = require "heart.Game"
+local WallModel = require "WallModel"
 
 function love.load()
-    game = Game.new()
+    game = Game.new({
+        cameraScale = 0.1,
+    })
     game:setWorldViewEnabled(true)
 
     game:setModelCreator("ball", BallModel.new)
+    game:setModelCreator("wall", WallModel.new)
 
-    game:newModel("ball")
+    game:newModel("ball", {radius = 0.5})
+
+    game:newModel("wall", {
+        size = {20, 1},
+        position = {0, 10},
+    })
+    game:newModel("wall", {
+        size = {1, 20},
+        position = {-10, 0},
+    })
+    game:newModel("wall", {
+        size = {1, 20},
+        position = {10, 0},
+    })
 end
 
 function love.update(dt)

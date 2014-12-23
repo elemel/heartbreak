4fb5791e6c317a5316d453f43b85f6549cb44f5d
Mikael Lind <elemel@elemel.se>
2014-12-23 08:23:39 +0100

    Noisy brick colors

--- BrickView.lua
+++ BrickView.lua
@@ -12,8 +12,9 @@ function BrickView.new(game, model)
 end
 
 function BrickView:create()
+    local x0, y0 = unpack(self._model:getConfig().position or {0, 0})
     local width, height = self._model:getSize()
-    local heat = love.math.random()
+    local heat = heart.math.fbm2(0.1 * x0, 0.1 * y0)
     self._sprite = heart.graphics.newSprite({
         color = {heart.math.toByte4(heat + 0.5, heat, heat - 0.5, 1)},
         origin = {1 + 0.5 * width, 1 + 0.5 * height},
